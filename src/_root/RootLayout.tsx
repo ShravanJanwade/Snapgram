8fb4c8f550a7a745306c7bf947521224fca3baf1
Shravankumar Janawade <shravanjanwade2252@gmail.com>
2025-12-16 21:50:57 -0500

    Fixed cookie fallback login session issue

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,11 +1,19 @@
+import { useUserContext } from "@/context/AuthContext";
+import React from "react";
+import { Outlet, Navigate } from "react-router-dom";
 import Bottombar from "@/components/shared/Bottombar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
 import Topbar from "@/components/shared/Topbar";
-import { Button } from "@/components/ui/button";
-import React from "react";
-import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
+  const { isAuthenticated, isLoading } = useUserContext();
+
+  if (isLoading) return null;
+
+  if (!isAuthenticated) {
+    return <Navigate to="/sign-in" />;
+  }
+
   return (
     <div className="w-full md:flex">
       <Topbar />
